9d2d3d8ac15f4bc1ccf66149374243948aad6e1c
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-27 02:00:38 +0530

    Birwise operator

---
+++ lec11/BitwiseOperator.java
@@ -0,0 +1,11 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        System.out.println(5&6);//4
+        System.out.println(5|6);//7
+        System.out.println(5^6);//3
+        System.out.println(~5);//2
+        System.out.println(5<<2);//20
+        System.out.println(5>>2);//1
+
+    }
+}
