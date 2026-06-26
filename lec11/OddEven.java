137a2a54b6556a345172b2296da85cbf76775aff
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-27 02:16:03 +0530

    Birwise operator used to find even and odd

---
+++ lec11/OddEven.java
@@ -0,0 +1,11 @@
+public class OddEven {
+  public static void main(String[] args) {
+      int n=9;
+      int comparator=1;
+      if((n&comparator)==1){
+        System.out.println("odd");
+      }else{
+        System.out.println("even");
+      }
+  }
+}
