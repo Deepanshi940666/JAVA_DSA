516208851b50b6ddc5a7c7bf1afd4607f26fa6d8
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-29 00:49:44 +0530

    Birwise operations

--- lec11/OperationsOnBits.java
+++ lec11/OperationsOnBits.java
@@ -20,11 +20,17 @@ public class OperationsOnBits {
         // n=clearith(n, i);
         return (n&((~0)<<i));
     }
+    public static int clearrange(int n, int i,int j) {
+        int a=((~0)<<j+1);
+        int b=(1<<i)-1;
+        return (n&((a|b)));
+    }
     public static void main(String[] args) {
         System.out.println(getith(10, 2));
         System.out.println(setith(10, 2));
         System.out.println(clearith(10, 1));
         System.out.println(updateith(10, 2,1));
         System.out.println(clearFromith(15, 2));
+        System.out.println(clearrange(15, 1,4));
     }
 }
