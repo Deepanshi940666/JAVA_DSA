f5e043b057185ddda8d0cbf62210b2b4a21bca59
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-29 01:24:00 +0530

    count setbit in number

---
+++ lec11/countSetBit.java
@@ -0,0 +1,18 @@
+public class countSetBit {
+    public static void main(String[] args) {
+        int n = 10;
+        int count = 0;
+
+        while (n > 0) {
+            int LSB = n & 1;
+
+            if (LSB != 0) {
+                count++;
+            }
+
+            n = n >> 1;   // Shift every time
+        }
+
+        System.out.println(count);
+    }
+}
