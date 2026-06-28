872019c72eaf79a8c102f1565d0fa04eb2e48969
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-06-29 00:59:22 +0530

    Check given no is power of 2 or not

---
+++ lec11/CheckPowOf2.java
@@ -0,0 +1,12 @@
+public class CheckPowOf2 {
+    public static boolean check(int n) {
+        if((n&(n-1))==0){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(check(16)); //true
+        System.out.println(check(12)); //false
+    }
+}
