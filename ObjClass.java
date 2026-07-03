2cab1049559e692abf19a6593628870cc2beb823
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-07-03 23:08:52 +0530

    Object and classes

---
+++ ObjClass.java
@@ -0,0 +1,21 @@
+public class ObjClass {
+    public static void main(String[] args) {
+        Student s1= new Student(); //object
+        System.out.println(s1.name);
+        System.out.println(s1.rollno);
+        s1.setName("Deeps");
+        System.out.println(s1.name);
+    }
+}
+
+class Student{   //class
+    String name="Deepanshi";
+    int rollno = 108;
+
+    void setName(String newName){
+        name= newName;
+    }
+    void setRollno(int newRoll){
+        rollno=newRoll;
+    }
+}
