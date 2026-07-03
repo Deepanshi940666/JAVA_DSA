15241c075191413a40b6c5528521d5e0922f636d
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-07-03 23:51:17 +0530

    Access Modifier

---
+++ lec12/AccessModifier.java
@@ -0,0 +1,18 @@
+public class AccessModifier {
+    public static void main(String[] args) {
+        BackAccount acc1=new BackAccount();
+        System.out.println(acc1.Username);
+        acc1.setPassword(1234567);
+        // System.out.println(acc1.password);
+    }
+}
+
+class BackAccount{
+    public String Username="Deepanshi Gupta";
+    private int password =12345;
+
+    public void setPassword(int newpassword) {
+        password =newpassword;
+    }
+    
+}
