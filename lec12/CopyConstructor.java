20619236f2001c38ef457febc2beb94cee396b96
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-07-16 19:43:07 +0530

    Copy Constructor

--- lec12/CopyConstructor.java
+++ lec12/CopyConstructor.java
@@ -5,6 +5,9 @@ public class CopyConstructor {
         s1.name="Deeps";
         s1.roll=108;
         s1.house="blue";
+        s1.marks[0]=70;
+        s1.marks[1]=84;
+        s1.marks[2]=69;
         Student s2 =new Student(s1);
         s2.house= "red";
         //s1
@@ -16,6 +19,13 @@ public class CopyConstructor {
         System.out.println(s2.name);
         System.out.println(s2.roll);
         System.out.println(s2.house);
+        for(int i=0;i<3;i++){
+            System.out.println(s2.marks[i]);
+        }
+
+        s1.marks[2]=76;
+        System.out.println("s1 "+s1.marks[2]);
+        System.out.println("s2 "+s2.marks[2]);
     }
 }
 
@@ -23,13 +33,17 @@ class Student{
     public String name;
     public int roll;
     public String house;
+    public int marks[];
 
     Student(Student s1){
+        marks= new int[3];
         this.name=s1.name;
         this.roll=s1.roll;
+        this.marks= s1.marks;
     }
 
     public Student() { 
+        marks= new int[3];
         System.out.println("Heyy your Constructor is called....");
     } 
     
