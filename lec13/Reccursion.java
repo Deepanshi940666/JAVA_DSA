7c7a22d2d8914a9d7a725edff06585db938c617c
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-09-12 23:54:46 +0530

    to print no 5 to 1  and 1 to 5 by recursion

--- lec13/Reccursion.java
+++ lec13/Reccursion.java
@@ -1,15 +1,28 @@
-// to print no 5 to 1 y recurrision
+// to print no 5 to 1 by recurision
 public class Reccursion {
     public static void PrintNo(int n) {
         if(n==0){
             return;
         }
-        System.out.println(n);
+        System.out.print(n);
         PrintNo(n-1);
         
     }
+
+    public static void Print1To5(int n) {
+        if(n==6){
+            return;
+        }
+
+        System.out.print(n);
+        Print1To5(n+1);
+    }
+
     public static void main(String[] args) {
         int n=5;
         PrintNo(n);
+        System.out.println(" ");
+        int a=1;
+        Print1To5(a);
     }
 }
