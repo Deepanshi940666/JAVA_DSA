3b3736345466de5157ea6b6e33de44984ebb48cd
Deepanshi Gupta <deepanshi102938@gmail.com>
2026-09-16 21:19:37 +0530

    to find Fibbonaci series of n no.

---
+++ lec13/FibbonaciSeries.java
@@ -0,0 +1,21 @@
+public class FibbonaciSeries {
+    public static void Fibbo(int a, int b, int n) {
+        int sum;
+        if(n==0){
+            return;
+        }
+        sum=a+b;
+        a=b;
+        b=sum;
+        System.out.println(sum);
+        Fibbo(a, b, n-1);
+    }
+
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=5;
+        Fibbo(a, b, n);
+    }
+}
+
